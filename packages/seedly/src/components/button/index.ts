996b8fe33b457eaede8cbf9b2e8e25e1d46d5c31
Louis Gollut <louis.gollut@liip.ch>
2025-06-19 10:30:27 +0200

    [Button] Expose inner components to enhance composability

--- packages/seedly/src/components/button/index.ts
+++ packages/seedly/src/components/button/index.ts
@@ -1,2 +1,12 @@
-export { Button, type ButtonProps, type ButtonTypes } from './button';
+export {
+  Button,
+  ButtonRoot,
+  ButtonLabel,
+  ButtonSlot,
+  type ButtonProps,
+  type ButtonTypes,
+  type ButtonRootProps,
+  type ButtonLabelProps,
+  type ButtonSlotProps,
+} from './button';
 export { buttonRecipe, type ButtonVariants } from './button.css';
