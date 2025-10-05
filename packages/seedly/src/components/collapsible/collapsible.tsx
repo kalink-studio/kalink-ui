c8c98c010b22b66a027131c6618f9b97a1e882ac
Louis Gollut <louis@kalink.ch>
2025-10-05 16:06:26 +0200

    feat(seedly): Add collapsible component

---
+++ packages/seedly/src/components/collapsible/collapsible.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+import { Root, Trigger } from '@radix-ui/react-collapsible';
+
+export const Collapsible = Root;
+export const CollapsibleTrigger = Trigger;
---
+++ packages/seedly/src/components/collapsible/index.ts
@@ -0,0 +1,3 @@
+export { Collapsible, CollapsibleTrigger } from './collapsible';
+export { CollapsibleContent } from './collapsible-content';
+export { collapsibleContent } from './collapsible-content.css';
